38ce102c56156567c1d393012994341486ab8718
Arifmaulana071005 <arifmaulana071005@gmail.com>
2026-03-17 02:42:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function welcome(){
-alert("Selamat datang di Portfolio Arif Maulana!");
-}
+const cards = document.querySelectorAll(".project-card");
+
+cards.forEach(card => {
+card.addEventListener("mouseover", () => {
+card.style.background = "#334155";
+});
+
+card.addEventListener("mouseout", () => {
+card.style.background = "#1e293b";
+});
+});
